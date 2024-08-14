1e99b7a2f8d52ac4c81ae1e4ac8b30e2a3ae1b6e
nothingmin <wnl1012@naver.com>
2024-08-15 00:15:03 +0900

    chore(monitor): chore move directory

--- monitor/scrapper/tests/bobae.py
+++ monitor/scrapper/tests/bobae.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
-from scrapper.sites import bobae
+from monitor.scrapper.sites import bobae
 
 if __name__ == '__main__':
     print(bobae.main({
-        "duration" : 6
+        "duration": 6
     }, {}, webdriver.Chrome()))
