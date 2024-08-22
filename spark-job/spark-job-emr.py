93f813769ced334017437afee0e36a927ea98403
nothingmin <wnl1012@naver.com>
2024-08-22 18:19:57 +0900

    fix(spark-job): read extract data from s3

--- spark-job/spark-job-emr.py
+++ spark-job/spark-job-emr.py
@@ -1,18 +1,15 @@
 import json
+import sys
 
 import boto3
 from pyspark import SparkConf, SQLContext
 from pyspark.sql import SparkSession, Row
 from pyspark.sql.dataframe import DataFrame
+from datetime import datetime
 import pyspark.pandas as ps
 import numpy as np
-import datetime
 
 ps.set_option('compute.ops_on_diff_frames', True)
-
-# from S3, to S3 (To be updated)
-POST_FP = "s3://emr-temp-resource/clien_posts.csv"  # 크롤링 결과 파일 위치
-COMMENT_FP = "s3://emr-temp-resource/clien_comments.csv"  # 크롤링 결과 파일 위치
 
 MAX_COMMENTS_NUM = 200
 TIME_INTERVAL = 5 * 60  # seconds
@@ -22,6 +19,8 @@ def find_hot_criteria(row, model_df):
     condition = ((model_df['post_type'] == 1) \
                  & (model_df['relative_time'] == row.relatvie_time) \
                  & (model_df['cumulative_num'] == row.comments))
+    print(row.relatvie_time)
+    print(row.comments)
     hot_pdf_value = model_df[condition].pdf.values[0]
     return hot_pdf_value
 
@@ -30,6 +29,8 @@ def find_cold_criteria(row, model_df):
     condition = ((model_df['post_type'] == 0) \
                  & (model_df['relative_time'] == row.relatvie_time) \
                  & (model_df['cumulative_num'] == row.comments))
+    print(row.relatvie_time)
+    print(row.comments)
     hot_pdf_value = model_df[condition].pdf.values[0]
     return hot_pdf_value
 
@@ -41,15 +42,6 @@ def predict_post_type(post_df, model_df):
     post_df['post_type'] = 0
     post_df.loc[post_df['impact'] > 0, 'post_type'] = 1
     return post_df
-
-
-def set_to_nearest_half_hour():
-    collected_at = datetime.datetime.now().replace(second=0, microsecond=0)
-    if collected_at.minute >= 30:
-        collected_at = collected_at.replace(minute=30)
-    else:
-        collected_at = collected_at.replace(minute=00)
-    return collected_at
 
 
 def parse_date(date_str):
@@ -72,7 +64,7 @@ def read_redshift(spark: SparkSession, dbtable, db_user, db_password, query=None
     sql_context = SQLContext(spark.sparkContext)
     url = "jdbc:redshift://de3-workgroup.181252290322.ap-northeast-2.redshift-serverless.amazonaws.com:5439/dev?user=" + db_user + "&password=" + db_password
     # Access to Redshift cluster using Spark
-    
+
     if query is None:
         rows = sql_context.read \
             .format("io.github.spark_redshift_community.spark.redshift") \
@@ -110,7 +102,7 @@ def write_redshift(df: DataFrame, dbtable, db_user, db_password):
          .option("aws_iam_role","arn:aws:iam::181252290322:role/de3-team-project-Load-9ODBABB6O-RedshiftDefaultRole-JDX7MjpYhApU")
          .mode("overwrite")
          .save())
-        
+
     else:
         (df.write
          .format("io.github.spark_redshift_community.spark.redshift")
@@ -121,21 +113,28 @@ def write_redshift(df: DataFrame, dbtable, db_user, db_password):
          .option("aws_iam_role","arn:aws:iam::181252290322:role/de3-team-project-Load-9ODBABB6O-RedshiftDefaultRole-JDX7MjpYhApU")
          .mode("append")
          .save())
-    return 
+    return
 
 if __name__ == "__main__":
+    batch_id = sys.argv[1]
+    collected_at = datetime.strptime(batch_id, '%Y%m%d%H%M')
+    # TODO: make merge all extracted posts and comments
+    string_collected_at = collected_at.strftime("%Y-%m-%d")
+    POST_FP = f"s3://de3-extract/bobae/{string_collected_at}/posts/{batch_id}.csv"  # 크롤링 결과 파일 위치
+    COMMENT_FP = f"s3://de3-extract/bobae/{string_collected_at}/comments/{batch_id}.csv"  # 크롤링 결과 파일 위치
+
     spark = SparkSession.builder \
         .appName('transform') \
         .enableHiveSupport() \
         .getOrCreate()
-    db_user, db_password = get_db_credentials()
+
     post_df = (spark.read
                .option("header", True)
                .option("quote", "\"")
                .option("escape", "\"")
                .option("multiline", "true")
                .option("sep", ",")
-               .csv(POST_FP, sep=',', )
+               .csv(POST_FP, sep=',')
                .to_pandas_on_spark())
     comment_df = (spark.read
                   .option("header", True)
@@ -144,7 +143,9 @@ if __name__ == "__main__":
                   .csv(COMMENT_FP, sep=',')
                   .to_pandas_on_spark())
 
+    db_user, db_password = get_db_credentials()
     model_df = read_redshift(spark, "model", db_user, db_password)
+
     # preprocessing
     post_df['created_at'] = post_df['created_at'].apply(parse_date)
     comment_df.columns = ['cmt_author', 'cmt_count', 'post_id', 'cmt_created_at']
@@ -159,8 +160,7 @@ if __name__ == "__main__":
     number_of_comment_per_post_df = number_of_comment_per_post_df.reset_index()
     number_of_comment_per_post_df.loc[number_of_comment_per_post_df['comments'] > MAX_COMMENTS_NUM] = MAX_COMMENTS_NUM
 
-    # Add columns to classify the type of post.
-    collected_at = set_to_nearest_half_hour()
+
 
     # collected_at = ps.to_datetime('2023-10-31 00:00:52').replace(second=0, microsecond=0)  # from S3 (crawling)
     # cutoff_time = collected_at - datetime.timedelta(hours=12)
@@ -169,18 +169,20 @@ if __name__ == "__main__":
     # post_df = post_df[post_df['created_at'] <= collected_at]
     # comment_df = comment_df[comment_df['cmt_created_at'] <= collected_at]
 
+    # Add columns to classify the type of post.
     post_df = ps.merge(post_df, number_of_comment_per_post_df, left_on='id', right_on='post_id', how='left')
-    post_df = post_df.drop(columns=['id','updated_at'])
+    post_df = post_df.drop(columns=['id', 'updated_at'])
     post_df['relatvie_time'] = ((collected_at - post_df['created_at']) // TIME_INTERVAL) * 5
     post_df = predict_post_type(post_df, model_df)
     post_df['sns_id'] = 0  # TODO: use sns_id from input data. (to be deleted)
     post_df['collected_at'] = collected_at
-    post_df['batch_id'] = int(collected_at.strftime('%Y%m%d%H%M'))
+    post_df['batch_id'] = int(batch_id)
     post_df['likes'] = post_df['likes'].astype(int)
     post_df['views'] = post_df['views'].astype(int)
-    
+
+
     # Save result
-    write_redshift(post_df.to_spark(), 'view', db_user, db_password)
+    write_redshift(post_df.to_spark(), 'post', db_user, db_password)
 
     # Update view table
     query = """
