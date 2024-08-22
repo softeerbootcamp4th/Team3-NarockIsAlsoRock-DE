b0481ffc973a9e61e808343a9cb0e93219bb43bd
Gu Lee <99sphere@gmail.com>
2024-08-22 18:47:39 +0900

    fix(spark-job): fix the error that occurs when no comments exist.

--- spark-job/spark-job-emr.py
+++ spark-job/spark-job-emr.py
@@ -19,8 +19,8 @@ def find_hot_criteria(row, model_df):
     condition = ((model_df['post_type'] == 1) \
                  & (model_df['relative_time'] == row.relatvie_time) \
                  & (model_df['cumulative_num'] == row.comments))
-    print(row.relatvie_time)
-    print(row.comments)
+    # print(row.relatvie_time)
+    # print(row.comments)
     hot_pdf_value = model_df[condition].pdf.values[0]
     return hot_pdf_value
 
@@ -29,8 +29,8 @@ def find_cold_criteria(row, model_df):
     condition = ((model_df['post_type'] == 0) \
                  & (model_df['relative_time'] == row.relatvie_time) \
                  & (model_df['cumulative_num'] == row.comments))
-    print(row.relatvie_time)
-    print(row.comments)
+    # print(row.relatvie_time)
+    # print(row.comments)
     hot_pdf_value = model_df[condition].pdf.values[0]
     return hot_pdf_value
 
@@ -39,6 +39,7 @@ def predict_post_type(post_df, model_df):
     per_post_hot_probability = post_df.apply(lambda x: find_hot_criteria(x, model_df), axis=1)
     per_post_cold_probability = post_df.apply(lambda x: find_cold_criteria(x, model_df), axis=1)
     post_df['impact'] = np.log(per_post_hot_probability / per_post_cold_probability).round(2)
+    post_df['impact'] = post_df['impact'].replace(np.inf, 554.93) # set upper bound of impact 
     post_df['post_type'] = 0
     post_df.loc[post_df['impact'] > 0, 'post_type'] = 1
     return post_df
@@ -129,29 +130,30 @@ if __name__ == "__main__":
         .getOrCreate()
 
     post_df = (spark.read
-               .option("header", True)
-               .option("quote", "\"")
-               .option("escape", "\"")
-               .option("multiline", "true")
-               .option("sep", ",")
-               .csv(POST_FP, sep=',')
-               .to_pandas_on_spark())
+            .option("header", True)
+            .option("quote", "\"")
+            .option("escape", "\"")
+            .option("multiline", "true")
+            .option("sep", ",")
+            .csv(POST_FP, sep=',')
+            .to_pandas_on_spark())
     comment_df = (spark.read
-                  .option("header", True)
-                  .option("quote", "\"")
-                  .option("multiline", "true")
-                  .csv(COMMENT_FP, sep=',')
-                  .to_pandas_on_spark())
+                .option("header", True)
+                .option("quote", "\"")
+                .option("multiline", "true")
+                .csv(COMMENT_FP, sep=',')
+                .to_pandas_on_spark())
 
     db_user, db_password = get_db_credentials()
     model_df = read_redshift(spark, "model", db_user, db_password)
 
     # preprocessing
     post_df['created_at'] = post_df['created_at'].apply(parse_date)
-    comment_df.columns = ['cmt_author', 'cmt_count', 'post_id', 'cmt_created_at']
     comment_df['cmt_created_at'] = comment_df['cmt_created_at'].apply(parse_date)
     model_df['pdf'] = model_df['pdf'].astype(float)
     model_df['cumulative_num'] = model_df['cumulative_num'].astype(int)
+
+
     model_df['post_type'] = model_df['post_type'].astype(int)
 
     # get number of comments per post
@@ -159,8 +161,6 @@ if __name__ == "__main__":
         {'cmt_author': 'count'}).rename(columns={'cmt_author': 'comments'})
     number_of_comment_per_post_df = number_of_comment_per_post_df.reset_index()
     number_of_comment_per_post_df.loc[number_of_comment_per_post_df['comments'] > MAX_COMMENTS_NUM] = MAX_COMMENTS_NUM
-
-
 
     # collected_at = ps.to_datetime('2023-10-31 00:00:52').replace(second=0, microsecond=0)  # from S3 (crawling)
     # cutoff_time = collected_at - datetime.timedelta(hours=12)
@@ -171,6 +171,9 @@ if __name__ == "__main__":
 
     # Add columns to classify the type of post.
     post_df = ps.merge(post_df, number_of_comment_per_post_df, left_on='id', right_on='post_id', how='left')
+    post_df['comments'] = post_df['comments'].fillna(0)
+
+
     post_df = post_df.drop(columns=['id', 'updated_at'])
     post_df['relatvie_time'] = ((collected_at - post_df['created_at']) // TIME_INTERVAL) * 5
     post_df = predict_post_type(post_df, model_df)
@@ -181,20 +184,20 @@ if __name__ == "__main__":
     post_df['views'] = post_df['views'].astype(int)
 
 
-    # Save result
-    write_redshift(post_df.to_spark(), 'post', db_user, db_password)
+    # # Save result
+    # write_redshift(post_df.to_spark(), 'post', db_user, db_password)
 
-    # Update view table
-    query = """
-    WITH ranked_posts AS (
-        SELECT *,
-            ROW_NUMBER() OVER (PARTITION BY post_id ORDER BY batch_id DESC) AS rn
-        FROM post
-        WHERE created_at >= DATEADD('month', -1, CURRENT_DATE)
-    )
-    SELECT *
-    FROM ranked_posts
-    WHERE rn = 1
-    """
-    view_df = read_redshift(spark, "post", db_user, db_password, query)
-    write_redshift(view_df, 'view', db_user, db_password)
+    # # Update view table
+    # query = """
+    # WITH ranked_posts AS (
+    #     SELECT *,
+    #         ROW_NUMBER() OVER (PARTITION BY post_id ORDER BY batch_id DESC) AS rn
+    #     FROM post
+    #     WHERE created_at >= DATEADD('month', -1, CURRENT_DATE)
+    # )
+    # SELECT *
+    # FROM ranked_posts
+    # WHERE rn = 1
+    # """
+    # view_df = read_redshift(spark, "post", db_user, db_password, query)
+    # write_redshift(view_df, 'view', db_user, db_password)
