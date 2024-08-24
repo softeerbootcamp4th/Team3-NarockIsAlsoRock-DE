849b915f346230c1e5e1d0b0c6f87b2c08e27ca5
nothingmin <wnl1012@naver.com>
2024-08-24 20:50:19 +0900

    test(extract): add unit tests

--- infra/extract/sites/bobae.py
+++ infra/extract/sites/bobae.py
@@ -15,28 +15,31 @@ def main(event, context, driver: WebDriver):
     duration = event.get('duration', 12)
     path = f"https://www.bobaedream.co.kr/list.php?code=national&pagescale=70&page=1"
     driver.get(path)
+
     WebDriverWait(driver, 2).until(expected_conditions.element_to_be_clickable(
         (By.CSS_SELECTOR, '#boardlist')))
-    posts = driver.find_element(By.CSS_SELECTOR, '#boardlist').find_element(By.TAG_NAME, "tbody").find_elements(
-        By.TAG_NAME, "tr")
-    for post in posts:
-        if post.get_attribute("class") == "best":
-            continue
-        post.find_element(By.TAG_NAME, "a").click()
-        break
+
+    driver = click_latest_post(driver)
+
     posts_parsed, comments_parsed = [], []
     current_time = datetime.now()
     while True:
-        if is_post_exist(driver):
-            created_at = extract_created_at(driver)
-            if not current_time - timedelta(hours=duration) <= created_at:
-                break
-            posts_data, comments_data = post_crawling(driver)
-            if posts_data is not None:
-                posts_parsed.append(posts_data)
-                comments_parsed.extend(comments_data)
-        next_button = driver.find_element(By.CLASS_NAME, "topBtnGroup").find_element(By.XPATH, "//a[text()='다음글']")
-        next_button.click()
+        if not is_post_exist(driver):
+            driver = move_to_next_post(driver)
+            continue
+
+        created_at = parse_created_at(driver)
+        if should_stop(created_at, current_time, duration):
+            break
+
+        post = parse_post(driver)
+        comments = parse_comments(driver)
+        if post is not None:
+            posts_parsed.append(post)
+            comments_parsed.extend(comments)
+
+        driver = move_to_next_post(driver)
+
     driver.quit()
     return {
         "posts": posts_parsed,
@@ -44,57 +47,45 @@ def main(event, context, driver: WebDriver):
     }
 
 
+def should_stop(created_at, current_time, duration):
+    return current_time - timedelta(hours=duration) > created_at
+
+
+def click_latest_post(driver):
+    posts = driver.find_element(By.CSS_SELECTOR, '#boardlist').find_element(By.TAG_NAME, "tbody").find_elements(
+        By.TAG_NAME, "tr")
+    for post in posts:
+        if post.get_attribute("class") == "best":
+            continue
+        post.find_element(By.TAG_NAME, "a").click()
+        break
+    return driver
+
+def move_to_next_post(driver):
+    next_button = driver.find_element(By.CLASS_NAME, "topBtnGroup").find_element(By.XPATH, "//a[text()='다음글']")
+    next_button.click()
+    return driver
+
+
 def is_post_exist(driver):
     try:
         driver.find_element(By.CLASS_NAME, "countGroup")
         return True
-    except TimeoutException:
+    except:
         return False
 
 
-def extract_created_at(driver):
+def parse_created_at(driver):
     # 요소에서 텍스트를 가져오고 필요 없는 부분을 제거
     created_at_str = driver.find_element(By.CLASS_NAME, "countGroup").text.split("|")[-1].strip()
-
     # 괄호와 괄호 안의 문자 삭제
     cleaned_str = re.sub(r'\s*\(.*?\)', '', created_at_str)
-
     # datetime 객체로 변환
     created_at = datetime.strptime(cleaned_str, "%Y.%m.%d %H:%M")
-
     return created_at
 
 
-
-def cmt_crawling(driver, post_id):
-    try:
-        basiclist = driver.find_element(By.ID, 'cmt_reply')
-    except:
-        # 댓글이 없는 경우임
-        return []
-    comments = []
-    if basiclist is not None:
-        lis = basiclist.find_elements(By.TAG_NAME, 'li')
-
-    for li in lis:
-        try:
-            content = li.find_element(By.TAG_NAME, 'dd').text
-            content = re.sub(',', '', content)
-            content = re.sub('\n', '', content)
-            author = li.find_element(By.CLASS_NAME, 'author').text
-            date = li.find_element(By.CLASS_NAME, 'date').text
-            comments.append({
-                "post_id": post_id,
-                "cmt_content": content,
-                "cmt_author": author,
-                "cmt_created_at": date,
-            })
-        except:
-            pass
-    return comments
-
-
-def post_crawling(driver):
+def parse_post(driver):
     id, title, content, likes, views, created_at, updated_at = None, None, None, None, None, None, None
     url = driver.current_url
     title = driver.find_element(By.XPATH,
@@ -109,11 +100,7 @@ def post_crawling(driver):
         content += p.text
 
     likes = driver.find_element(By.ID, "tempPublic").text
-    # URL을 파싱합니다
-    parsed_url = urlparse(url)
-    # 쿼리 문자열에서 파라미터를 추출합니다
-    parameters = parse_qs(parsed_url.query)
-    id = parameters['No'][0]
+    id = get_post_id(url)
     author = driver.find_element(By.CLASS_NAME, 'nickName').text
     views = driver.find_element(By.XPATH,
                                 '/html/body/div[1]/div[6]/div/div/div[1]/div[3]/div[1]/div[1]/dl/dt/span/em[1]').text
@@ -149,9 +136,19 @@ def post_crawling(driver):
         'updated_at': updated_at,
     }
 
-    #######################
-    # 댓글 크롤링
-    #######################
+    return post_parsed
+
+
+def get_post_id(url):
+    # URL을 파싱합니다
+    parsed_url = urlparse(url)
+    # 쿼리 문자열에서 파라미터를 추출합니다
+    parameters = parse_qs(parsed_url.query)
+    id = parameters['No'][0]
+    return id
+
+
+def parse_comments(driver):
     comments_parsed = []
     comments = cmt_crawling(driver, post_id=id)
     comments_parsed.extend(comments)
@@ -167,11 +164,38 @@ def post_crawling(driver):
         cmt_page = int(cmt_page)
     except:
         pass
-
     if cmt_page > 1:
         for i in range(cmt_page - 1, 0, -1):
             driver.execute_script(f"javascript:sel_cmt_paging('{i}');")
             comments = cmt_crawling(driver, post_id=id)
             comments_parsed.extend(comments)
 
-    return post_parsed, comments_parsed
+    return comments_parsed
+
+
+def cmt_crawling(driver, post_id):
+    try:
+        basiclist = driver.find_element(By.ID, 'cmt_reply')
+    except:
+        # 댓글이 없는 경우임
+        return []
+    comments = []
+    if basiclist is not None:
+        lis = basiclist.find_elements(By.TAG_NAME, 'li')
+
+    for li in lis:
+        try:
+            content = li.find_element(By.TAG_NAME, 'dd').text
+            content = re.sub(',', '', content)
+            content = re.sub('\n', '', content)
+            author = li.find_element(By.CLASS_NAME, 'author').text
+            date = li.find_element(By.CLASS_NAME, 'date').text
+            comments.append({
+                "post_id": post_id,
+                "cmt_content": content,
+                "cmt_author": author,
+                "cmt_created_at": date,
+            })
+        except:
+            pass
+    return comments
---
+++ infra/extract/tests/test_bobae.py
@@ -0,0 +1,41 @@
+from unittest import TestCase
+from urllib.parse import urlparse, parse_qs
+
+from selenium import webdriver
+
+from extract.sites.bobae import parse_post, click_latest_post, get_post_id
+
+
+class Test(TestCase):
+
+    def setup_headless_driver(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument("--single-process")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
+        chrome_options.add_argument("disable-gpu")
+        driver = webdriver.Chrome(options=chrome_options)
+        return driver
+
+    def setUp(self):
+        self.post = {'id': '2323683', 'title': '1년,2년,3년전 탔던 차들 한번 나열해봤습니다..[21]',
+                     'content': "문득 1년전2년전3년전에는 이맘때 무슨 차를 타고 있었지?..생각이 들어서 한번 찾아봤습니다일단 현재 24년8월에는 K9 5.0 퀀텀을 타고 있고.. 작년 23년8월에는 W222 후기형 S400d를 타고 있었습니다.. 2년전 22년8월에는 W222 전기형 S350d를... 3년전 21년8월과 4년전 20년8월에는 제네시스 EQ900을..ㅡ.ㅡ;이렇게 보니 진짜 징하게도 바꿔가면서 탔네요 하하..그나마 EQ900을 좀 길게 탔네요..내년 8월에는 지금 타는 K9을 그대로 타고 있기를 희망해봅니다 ..'=';;",
+                     'likes': '45', 'url': 'https://www.bobaedream.co.kr/view?code=national&No=2323683&bm=1',
+                     'author': '프레기온', 'views': '4,018', 'created_at': '2024.08.24 13:07',
+                     'updated_at': '2024.08.24 13:08'}
+        self.driver = self.setup_headless_driver()
+        self.post_url = "https://www.bobaedream.co.kr/view?code=national&No=2323683&bm=1"
+        self.board_url = "https://www.bobaedream.co.kr/list?code=national"
+
+    def test_parse_post(self):
+        self.driver.get(self.post_url)
+        post = parse_post(self.driver)
+        self.assertEqual(post['id'], self.post['id'], "The dictionaries do not have the same keys and values")
+
+    def test_get_post_id(self):
+        id = get_post_id(self.post_url)
+        self.assertEqual(id, self.post['id'], "The post id does not match")
+
+
