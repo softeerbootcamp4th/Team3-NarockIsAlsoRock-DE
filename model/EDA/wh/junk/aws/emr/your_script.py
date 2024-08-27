877a235afbf69b62f19a7d8d6dbe4797eeff74d1
WooHyeong Choi <48896148+dn7638@users.noreply.github.com>
2024-08-27 10:29:41 +0900

    Update your_script.py

--- model/EDA/wh/junk/aws/emr/your_script.py
+++ model/EDA/wh/junk/aws/emr/your_script.py
@@ -3,9 +3,9 @@ from pyspark.sql import SparkSession
 # 1. Spark 세션 생성
 spark = SparkSession.builder \
     .appName("CSV to Parquet") \
-    .config("spark.hadoop.fs.s3a.access.key", "AKIASUM32O4JGGSJIKFX") \
-    .config("spark.hadoop.fs.s3a.secret.key", "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") \
-    .config("spark.hadoop.fs.s3a.endpoint", "s3.ap-northeast-2.amazonaws.com") \
+    .config("spark.hadoop.fs.s3a.access.key", "") \
+    .config("spark.hadoop.fs.s3a.secret.key", "") \
+    .config("spark.hadoop.fs.s3a.endpoint", "") \
     .getOrCreate()
 
 # 2. S3에서 CSV 파일 로드
